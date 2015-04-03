1bea808da51d36a3ad6a3c768924384cc036f405
Armando Amador <arm.amador@gmail.com>
2015-04-03 17:51:06 -0400

    Refactor the case statement method with state/strategy
     - created module for default pricing
     - created classes for type of pricing

--- video_store/movie.rb
+++ video_store/movie.rb
@@ -5,28 +5,31 @@ class Movie
   CHILDRENS = 2
 
   attr_reader :title
-  attr_accessor :price_code
+  attr_reader :price_code
+  attr_writer :price
+
+  def price_code=(value)
+    @price_code = value
+    @price =
+      case price_code
+      when REGULAR
+        RegularPrice.new
+      when NEW_RELEASE
+        NewReleasePrice.new
+      when CHILDRENS
+        ChildrensPrice.new
+      end
+  end
 
   def initialize(title, price_code)
     @title, @price_code = title, price_code
   end
 
   def charge(days_rented)
-    result = 0
-    case price_code
-    when REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when NEW_RELEASE
-      result += days_rented * 3
-    when CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    @price.charge(days_rented)
   end
 
   def frequent_renter_points(days_rented)
-    (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
+    @price.frequent_renter_points(days_rented)
   end
 end
