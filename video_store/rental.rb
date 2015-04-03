932f9ba19a60774509b241645681866900933e27
Armando Amador <arm.amador@gmail.com>
2015-04-03 15:53:07 -0400

    Start sample program that calculate and print a statement of customer’s charges at a video store.

---
+++ video_store/rental.rb
@@ -0,0 +1,8 @@
+# Rental class represents a customer renting a movie
+class Rental
+  attr_reader :movie, :days_rented
+
+  def initialize(movie, days_rented)
+    @movie, @days_rented = movie, days_rented
+  end
+end
